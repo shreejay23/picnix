f65bf83f53357f41461e88aeba1ed6ed8b2050e7
prajjawal05 <prajjawalagarwal@gmail.com>
2023-12-04 00:08:58 -0500

    reverted to normal file name

--- picnix/settings.py
+++ picnix/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "picnix.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db_local.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
