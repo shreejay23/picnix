6184780ea8e8a698d98f705e6590592a7e3fb0f6
shreejay23 <shreejay.va@gmail.com>
2023-12-04 02:28:21 -0500

    Added Exact Match Detection

--- picnix_processor/app_utils.py
+++ picnix_processor/app_utils.py
@@ -1,5 +1,5 @@
 import os
-
+import cv2
 
 def get_image_path(uploaded_image):
     directory = uploaded_image.image.url[1:].split('/')
@@ -22,3 +22,25 @@ def get_cluster_center_in_nums(cluster_center_text):
     text = cluster_center_text.split(',')
     nums = list(float(x) for x in text)
     return nums
+
+def is_exactly_same(image1_path, image2_path) -> bool:
+    """
+    Params:
+        image1_path and image2_path
+    
+    Returns:
+        bool: True if the images are exactly the same, False otherwise
+    
+    Obtains the pixel-wise difference between the two images and checks if the difference(in all three color channel) is 0 or not and returns accordingly.
+    """
+    image1 = cv2.imread(image1_path)
+    image2 = cv2.imread(image2_path)
+    if image1.shape == image2.shape:
+        difference = cv2.subtract(image1, image2) # This operation calculates the pixel-wise difference between the two images.
+        blue, green, red = cv2.split(difference) # Split the difference in three color channels: blue, green, and red.
+
+        # Check if all three color channels in the difference image have no nonzero (non-black) pixels.
+        if cv2.countNonZero(red) == 0 and cv2.countNonZero(green) == 0 and cv2.countNonZero(blue) == 0:
+            return True
+
+    return False
--- picnix_processor/tasks.py
+++ picnix_processor/tasks.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 import numpy as np
 from celery import shared_task
 from sklearn.cluster import KMeans
@@ -10,11 +11,20 @@ from picnix_processor.app_utils import *
 from picnix_processor.ImageFeatures import ImageFeatures
 
 
-def identify_cluster(cluster_centers, test_moments):
-    distances = np.linalg.norm(np.array(cluster_centers) - np.array(test_moments), axis=1)
+def identify_cluster(cluster_centers, image_moments):
+    distances = np.linalg.norm(np.array(cluster_centers) - np.array(image_moments), axis=1)
     sorted_clusters = np.argsort(distances)
 
-    return distances, sorted_clusters
+    return distances, sorted_clusters[:2]
+
+def check_for_exact_image_match(cluster_label, image1_path):
+    imageClusters = models.ImageCluster.objects.filter(cluster_id = cluster_label)
+    for imageCluster in imageClusters:
+        image2 = get_object_or_404(models.Image, id=imageCluster.image_id)
+        image2_path = get_image_path(image2)
+        if is_exactly_same(image1_path, image2_path):
+            return True
+    return False
 
 def process_uploaded_image(image_id):
     image = get_object_or_404(models.Image, id=image_id)
@@ -28,61 +38,65 @@ def process_uploaded_image(image_id):
     # print("LABELS")
     # print(cluster_labels)
 
-    test_image_processed = ImageFeatures().preprocess_image(img_path)
-    test_moments = ImageFeatures().calculate_moment_invariants(test_image_processed)
+    processed_image = ImageFeatures().preprocess_image(img_path)
+    image_moments = ImageFeatures().calculate_moment_invariants(processed_image)
     n = len(cluster_labels)
 
     if n < 1:
-        test_image_cluster_label = n + 1
-        new_clusterInfo = models.ClusterInfo(cluster_id = test_image_cluster_label, cluster_center = get_cluster_center_in_text(test_moments))
+        image_cluster_label = n + 1
+        new_clusterInfo = models.ClusterInfo(cluster_id = image_cluster_label, cluster_center = get_cluster_center_in_text(image_moments))
         new_clusterInfo.save()
 
-        test_imageCluster = models.ImageCluster(cluster_id = test_image_cluster_label, image_id = image.id)
-        test_imageCluster.save()
+        imageCluster = models.ImageCluster(cluster_id = image_cluster_label, image_id = image.id)
+        imageCluster.save()
 
-        return JsonResponse({'message': 'Image processed successfully'})
+        return image_cluster_label
 
-    scores, sorted_clusters_indices = identify_cluster(cluster_centers, test_moments)
+    scores, sorted_clusters_indices = identify_cluster(cluster_centers, image_moments)
 
     # print("Scores:")
     # print(scores)
 
-    i = 0
-    test_image_cluster_label = 0
+    ind = sorted_clusters_indices[0]
+    exact_match_flag = False
+    if scores[sorted_clusters_indices[-1]] <= threshold and check_for_exact_image_match(cluster_labels[sorted_clusters_indices[-1]], img_path):
+        ind = sorted_clusters_indices[-1]
+        exact_match_flag = True
+    elif check_for_exact_image_match(cluster_labels[sorted_clusters_indices[0]], img_path):
+        exact_match_flag = True
+
     n = len(cluster_labels)
     flag = False
-    while i < n:
-        ind = sorted_clusters_indices[i]
-        # print("IND " + str(ind))
-        cluster_score = scores[ind]
-        test_image_cluster_label = cluster_labels[ind]
-        if cluster_score > threshold:
-            flag = True
-            break
-        else:
-            near_duplicate_cluster = models.ClusterInfo.objects.get(cluster_id = test_image_cluster_label)
-            near_duplicate_cluster_centers = get_cluster_center_in_nums(near_duplicate_cluster.cluster_center)
-            # print("Cluster centers")
-            prev_cluster_length = models.ImageCluster.objects.filter(cluster_id = test_image_cluster_label).count()
-            # return JsonResponse({'message': 'NOOOOOOOOOOOOOOOOOOOO'})
-            temp_numerator = [prev_cluster_length * num for num in near_duplicate_cluster_centers]
-            for i in range(len(test_moments)):
-                temp_numerator[i] += test_moments[i]
-            new_cluster_centers = [num / (prev_cluster_length + 1) for num in temp_numerator]
-            models.ClusterInfo.objects.filter(cluster_id = test_image_cluster_label).update(cluster_center = get_cluster_center_in_text(new_cluster_centers))
-            break
-        i += 1
+    # print("IND " + str(ind))
+    image_cluster_label = cluster_labels[ind]
 
-    if i == n or flag:
-        test_image_cluster_label = n + 1
-        new_clusterInfo = models.ClusterInfo(cluster_id = test_image_cluster_label, cluster_center = get_cluster_center_in_text(test_moments))
+    if scores[ind] > threshold:
+        flag = True
+    else:
+        near_duplicate_cluster = models.ClusterInfo.objects.get(cluster_id = image_cluster_label)
+        near_duplicate_cluster_centers = get_cluster_center_in_nums(near_duplicate_cluster.cluster_center)
+        # print("Cluster centers")
+        prev_cluster_length = models.ImageCluster.objects.filter(cluster_id = image_cluster_label).count()
+        temp_numerator = [prev_cluster_length * num for num in near_duplicate_cluster_centers]
+        for i in range(len(image_moments)):
+            temp_numerator[i] += image_moments[i]
+        new_cluster_centers = [num / (prev_cluster_length + 1) for num in temp_numerator]
+        models.ClusterInfo.objects.filter(cluster_id = image_cluster_label).update(cluster_center = get_cluster_center_in_text(new_cluster_centers))
+
+    if flag:
+        image_cluster_label = n + 1
+        new_clusterInfo = models.ClusterInfo(cluster_id = image_cluster_label, cluster_center = get_cluster_center_in_text(image_moments))
         new_clusterInfo.save()
 
-    print(str(test_image_cluster_label) + " is the cluster ID of the new image")
-    test_imageCluster = models.ImageCluster(cluster_id = test_image_cluster_label, image_id = image.id)
-    test_imageCluster.save()
+    print(str(image_cluster_label) + " is the cluster ID of the new image")
 
-    return JsonResponse({'message': 'Image processed successfully'})
+    if exact_match_flag:
+        print("The exact match flag is True")
+
+    imageCluster = models.ImageCluster(cluster_id = image_cluster_label, image_id = image.id)
+    imageCluster.save()
+
+    return image_cluster_label
 
 
 @shared_task
@@ -90,7 +104,8 @@ def process_task(image_id, **kwargs):
     # Your processing logic here
     print("Received image: {}".format(image_id))
     try:
-        process_uploaded_image(image_id)
+        print("Cluster Label: " + str(process_uploaded_image(image_id)))
         print("Processed image: {}".format(image_id))
     except Exception as e:
+        print(traceback.format_exc())
         print("Error processing image: {}".format(e))
